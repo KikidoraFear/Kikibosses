509f22b7e335673e900873a547c0b17df6cdea76
KikidoraFear <154637862+KikidoraFear@users.noreply.github.com>
2024-05-03 11:54:34 +0200

    code cleanup
    let's hope it didnt break stuff

--- Kikibosses.lua
+++ Kikibosses.lua
@@ -74,59 +74,48 @@ local function InTable(val, tbl)
   return false
 end
 
-local function StringInTable(val, tbl)
-  for _, value in pairs(tbl) do
-    if string.find(val, value) then
-        return true
-    end
-  end
-  return false
-end
-
-
 ----------
 -- Init --
 ----------
+local player_name = UnitName("player")
+local unitIDs = {"player"} -- unitID player
+for i=2,5 do unitIDs[i] = "party"..i-1 end -- unitIDs party
+for i=6,45 do unitIDs[i] = "raid"..i-5 end -- unitIDs raid
+local unitIDs_cache = {} -- init unitIDs_cache[name] = unitID
+
 local boss_mode = ""
-local boss_data = {
-  Loatheb = {
-    healers = {},
-    num_healers = 0
-  },
-  Fourhm = {}
-}
-
+local loatheb = {}
+local horsemen = {}
 
 ---------------------------------
--- 4 Horsemen Healing Rotation --
+-- Horsemen Healing Rotation --
 ---------------------------------
-local parser_4hm = CreateFrame("Frame")
-local debuff_detector_4hm = CreateFrame("Frame")
-parser_4hm:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE") -- 4hm mark event
-parser_4hm:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE") -- 4hm mark event
-parser_4hm:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE") -- 4hm mark event
-local debuff_detector_4hm_timer_start = -1
-local mark_counter = 0
-local mark_interval = 12
--- local mark_bosses = {"Zeliek","Mograine","Thane","Blaumeux"}
+horsemen.update_frame = CreateFrame("Frame")
+horsemen.event_frame = CreateFrame("Frame")
+horsemen.event_frame:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_DAMAGE") -- horsemen mark event
+horsemen.event_frame:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_DAMAGE") -- horsemen mark event
+horsemen.event_frame:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_DAMAGE") -- horsemen mark event
+horsemen.ti_mark_start = -1
+horsemen.ct_marks = 0
+horsemen.ti_mark_interval = 12
 
-debuff_detector_4hm:SetScript("OnUpdate", function()
-  if (boss_mode=="4hm") and (debuff_detector_4hm_timer_start>0) and (GetTime() > debuff_detector_4hm_timer_start + mark_interval*mark_counter) then
-    if (math.mod(mark_counter, 3)+1)==boss_data["Fourhm"] then
-      print("Mark: "..(mark_counter+1).." MOVE!")
+horsemen.update_frame:SetScript("OnUpdate", function()
+  if (boss_mode=="horsemen") and (horsemen.ti_mark_start>0) and (GetTime() > horsemen.ti_mark_start + horsemen.ti_mark_interval*horsemen.ct_marks) then
+    if (math.mod(horsemen.ct_marks, 3)+1)==horsemen.mark_player then
+      print("Mark: "..(horsemen.ct_marks+1).." MOVE!")
       PlaySoundFile("Interface\\AddOns\\Kikibosses\\SFX\\horsemen_move.mp3")
     else
-      print("Mark: "..(mark_counter+1).." STAY!")
+      print("Mark: "..(horsemen.ct_marks+1).." STAY!")
     end
-    mark_counter = mark_counter + 1
+    horsemen.ct_marks = horsemen.ct_marks + 1
   end
 end)
 
-parser_4hm:SetScript("OnEvent", function()
-  if (boss_mode == "4hm") and (debuff_detector_4hm_timer_start<0) then
+horsemen.event_frame:SetScript("OnEvent", function()
+  if (boss_mode == "horsemen") and (horsemen.ti_mark_start<0) then
     if string.find(arg1, " afflicted by Mark of ") then
-      debuff_detector_4hm_timer_start = GetTime()
-      mark_counter = 0
+      horsemen.ti_mark_start = GetTime()
+      horsemen.ct_marks = 0
     end
   end
 end)
@@ -135,30 +124,21 @@ end)
 ------------------------------
 -- Loatheb Healing Rotation --
 ------------------------------
-local loatheb_healing_spells = {
-  PRIEST = "Greater Heal",
-  DRUID = "Healing Touch",
-  SHAMAN = "Healing Wave",
-  PALADIN = "Holy Light"
-}
-local player_name = UnitName("player")
-local parser_loatheb = CreateFrame("Frame")
-local debuff_detector_loatheb = CreateFrame("Frame")
-local debuff_detector_timer = -1
-local rw_text = ""
-local unitIDs = {"player"} -- unitID player
-for i=2,5 do unitIDs[i] = "party"..i-1 end -- unitIDs party
-for i=6,45 do unitIDs[i] = "raid"..i-5 end -- unitIDs raid
-local unitIDs_cache = {} -- init unitIDs_cache[name] = unitID
+loatheb.healers = {}
+loatheb.spells = {"Greater Heal", "Healing Touch", "Healing Wave", "Holy Light"}
+loatheb.event_frame = CreateFrame("Frame")
+loatheb.update_frame = CreateFrame("Frame")
+loatheb.debuff_timer = -1
+loatheb.rw_text = ""
 -- DETECT HEALS
-parser_loatheb:RegisterEvent("CHAT_MSG_SPELL_SELF_BUFF")
-parser_loatheb:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS")
-parser_loatheb:RegisterEvent("CHAT_MSG_SPELL_FRIENDLYPLAYER_BUFF")
-parser_loatheb:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_BUFFS")
-parser_loatheb:RegisterEvent("CHAT_MSG_SPELL_HOSTILEPLAYER_BUFF")
-parser_loatheb:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_HOSTILEPLAYER_BUFFS")
-parser_loatheb:RegisterEvent("CHAT_MSG_SPELL_PARTY_BUFF")
-parser_loatheb:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_BUFFS")
+loatheb.event_frame:RegisterEvent("CHAT_MSG_SPELL_SELF_BUFF")
+loatheb.event_frame:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_SELF_BUFFS")
+loatheb.event_frame:RegisterEvent("CHAT_MSG_SPELL_FRIENDLYPLAYER_BUFF")
+loatheb.event_frame:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_FRIENDLYPLAYER_BUFFS")
+loatheb.event_frame:RegisterEvent("CHAT_MSG_SPELL_HOSTILEPLAYER_BUFF")
+loatheb.event_frame:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_HOSTILEPLAYER_BUFFS")
+loatheb.event_frame:RegisterEvent("CHAT_MSG_SPELL_PARTY_BUFF")
+loatheb.event_frame:RegisterEvent("CHAT_MSG_SPELL_PERIODIC_PARTY_BUFFS")
 
 local function MakeGfindReady(template) -- changes global string to fit gfind pattern
   template = gsub(template, "%%s", "(.+)") -- % is escape: %%s = %s raw
@@ -183,28 +163,28 @@ combatlog_patterns[7] = {string=MakeGfindReady(HEALEDCRITOTHEROTHER), order={1, 
 combatlog_patterns[8] = {string=MakeGfindReady(HEALEDOTHEROTHER), order={1, 2, 3, 4, nil}, kind="heal"} -- %s's %s heals %s for %d.
 -- combatlog_patterns[12] = {string=MakeGfindReady(PERIODICAURAHEALOTHEROTHER), order={3, 4, 1, 2, nil}, kind="heal"} -- %s gains %d health from %s's %s.
 
-debuff_detector_loatheb:SetScript("OnUpdate", function()
-  if (boss_mode=="loatheb") and (debuff_detector_timer>0) and (GetTime() > debuff_detector_timer + 0.2) then -- check debuff 200ms after heal has been cast (there's a bit of a delay for it to get applied)
-    for idx, healer in ipairs(boss_data["Loatheb"]["healers"]) do -- always start at 1 in the list and search first healer that can heal (so you can prioritise strong healers)
+loatheb.update_frame:SetScript("OnUpdate", function()
+  if (boss_mode=="loatheb") and (loatheb.debuff_timer>0) and (GetTime() > loatheb.debuff_timer + 0.2) then -- check debuff 200ms after heal has been cast (there's a bit of a delay for it to get applied)
+    for idx, healer in ipairs(loatheb.healers) do -- always start at 1 in the list and search first healer that can heal (so you can prioritise strong healers)
       local healer_id = GetUnitID(unitIDs_cache, unitIDs, healer)
       local healer_debuff = HasDebuff(healer_id, "Interface\\Icons\\Spell_Shadow_AuraOfDarkness") -- only works with icon link, idk
       -- local healer_debuff = HasDebuff(healer_id, "Interface\\Icons\\Spell_Holy_AshesToAshes") -- priest shield debuff for testing
       local healer_dead = UnitIsDeadOrGhost(healer_id)
 
       if (not healer_debuff) and (not healer_dead) then
-        rw_text = rw_text.." -> "..healer.." next"
+        loatheb.rw_text = loatheb.rw_text.." -> "..healer.." next"
         break
-      elseif idx == boss_data["Loatheb"]["num_healers"] then
-        rw_text = rw_text.." -> no healer available (heal as soon as you can)"
+      elseif idx == loatheb.num_healers then
+        loatheb.rw_text = loatheb.rw_text.." -> no healer available (heal as soon as you can)"
       end
     end
-    SendChatMessage(rw_text, "RAID_WARNING")
-    debuff_detector_timer = -1
+    SendChatMessage(loatheb.rw_text, "RAID_WARNING")
+    loatheb.debuff_timer = -1
   end
 end)
 
 
-parser_loatheb:SetScript("OnEvent", function()
+loatheb.event_frame:SetScript("OnEvent", function()
   if boss_mode=="loatheb" then
     local pars = {}
     for _,combatlog_pattern in ipairs(combatlog_patterns) do
@@ -230,14 +210,10 @@ parser_loatheb:SetScript("OnEvent", function()
           value = 0
         end
 
-        if InTable(source, boss_data["Loatheb"]["healers"]) then -- source == boss_data["Loatheb"]["healers"][idx_healer] then
-          local healer_id = GetUnitID(unitIDs_cache, unitIDs, source)
-          local _, healer_class = UnitClass(healer_id)
-          if loatheb_healing_spells[healer_class] == spell then
-            local eheal, oheal = EOHeal(unitIDs_cache, unitIDs, value, target)
-            rw_text = source.." used "..spell.." to heal "..target.." for "..eheal.." (+"..oheal..")"
-            debuff_detector_timer = GetTime()
-          end
+        if InTable(source, loatheb.healers) and InTable(spell, loatheb.spells) then
+          local eheal, oheal = EOHeal(unitIDs_cache, unitIDs, value, target)
+          loatheb.rw_text = source.." used "..spell.." to heal "..target.." for "..eheal.." (+"..oheal..")"
+          loatheb.debuff_timer = GetTime()
         end
         return
       end
@@ -254,41 +230,41 @@ SlashCmdList["KIKIBOSSES"] = function(msg)
   local _, _, cmd, args = string.find(msg, "%s?(%w+)%s?(.*)")
   if (msg == "" or msg == nil) then
     print("/kikibosses loatheb Kikidora#Slewdem#...")
-    print("/kikibosses 4hm 1,2,3")
+    print("/kikibosses horsemen 1,2,3")
   elseif cmd == "loatheb" then
     if boss_mode == "loatheb" then
       boss_mode = ""
       print("Kikibosses: Loatheb deactivated.")
     else
       boss_mode = "loatheb"
-      ClearTable(boss_data["Loatheb"]["healers"])
-      ParseHealers(args, boss_data["Loatheb"]["healers"])
-      boss_data["Loatheb"]["num_healers"] = GetArLength(boss_data["Loatheb"]["healers"])
+      ClearTable(loatheb.healers)
+      ParseHealers(args, loatheb.healers)
+      loatheb.num_healers = GetArLength(loatheb.healers)
       print("Kikibosses: Loatheb activated.")
     end
-  elseif cmd == "4hm" then
-    if boss_mode == "4hm" then
+  elseif cmd == "horsemen" then
+    if boss_mode == "horsemen" then
       boss_mode = ""
-      print("Kikibosses: 4 Horsemen deactivated.")
+      print("Kikibosses: Horsemen deactivated.")
     else
-      boss_mode = "4hm"
-      boss_data["Fourhm"] = tonumber(args)
-      debuff_detector_4hm_timer_start = -1
-      mark_interval = 12
-      print("Kikibosses: 4 Horsemen activated.")
-      mark_counter = 0
+      boss_mode = "horsemen"
+      horsemen.mark_player = tonumber(args)
+      horsemen.ti_mark_start = -1
+      horsemen.ti_mark_interval = 12
+      horsemen.ct_marks = 0
+      print("Kikibosses: Horsemen activated.")
     end
-  elseif cmd == "4hmtest" then
-    if boss_mode == "4hm" then
+  elseif cmd == "horsementest" then
+    if boss_mode == "horsemen" then
       boss_mode = ""
-      print("Kikibosses: 4 Horsemen deactivated.")
+      print("Kikibosses: Horsemen deactivated.")
     else
-      boss_mode = "4hm"
-      boss_data["Fourhm"] = tonumber(args)
-      print("Kikibosses: 4 Horsemen activated. Test enabled!")
-      mark_interval = 2
-      debuff_detector_4hm_timer_start = GetTime()
-      mark_counter = 0
+      boss_mode = "horsemen"
+      horsemen.mark_player = tonumber(args)
+      horsemen.ti_mark_start = GetTime()
+      horsemen.ti_mark_interval = 2
+      horsemen.ct_marks = 0
+      print("Kikibosses: Horsemen activated. Test enabled!")
     end
   end
 end
